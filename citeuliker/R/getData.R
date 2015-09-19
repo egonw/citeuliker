603429ec649230d3e63f610392871541b2107fb0
Egon Willighagen <egon.willighagen@gmail.com>
2015-09-19 15:07:54 +0200

    For convenience, convert the 'published' property to c('year','month','day')

--- citeuliker/R/getData.R
+++ citeuliker/R/getData.R
@@ -1,24 +1,23 @@
 getData <- function(group=NA, user=NA, paging=NA) {
+  handle = new_handle()
+  handle_setheaders(handle, "User-Agent" = "r/citeuliker")
   if (!is.na(group)) {
-    handle = new_handle()
-    handle_setheaders(handle, "User-Agent" = "r/citeuliker")
     url = paste("http://www.citeulike.org/json/group/", group, sep="")
-    conn <- curl::curl(url, handle, open="r")
-    txt <- readLines(conn)
-    close(conn)
-    return(fromJSON(txt))
-  }
-  if (!is.na(user)) {
-    handle = new_handle()
-    handle_setheaders(handle, "User-Agent" = "r/citeuliker")
+  } else if (!is.na(user)) {
     options = "";
     if (!is.na(paging)) {
       options = paste("?page=", paging[0], "&per_page=", paging[1], sep="")
     }
     url = paste("http://www.citeulike.org/json/user/", user, options, sep="")
-    conn <- curl::curl(url, handle, open="r")
-    txt <- readLines(conn)
-    close(conn)
-    return(fromJSON(txt))
+  } else {
+    stop("You must give a user or group identifier");
   }
+  conn <- curl::curl(url, handle, open="r")
+  txt <- readLines(conn)
+  close(conn)
+  data = fromJSON(txt)
+  data["year"] <- sapply(data["published"][[1]], function(row) { row[1] })
+  data["month"] <- sapply(data["published"][[1]], function(row) { row[2] })
+  data["day"] <- sapply(data["published"][[1]], function(row) { row[3] })
+  return(data)
 }
--- citeuliker/tests/testthat/test-getData.R
+++ citeuliker/tests/testthat/test-getData.R
@@ -3,10 +3,15 @@ context("Fetching")
 
 test_that("getting grouping data", {
   data = getData(group=19781)
-  expect_equal(length(names(data)), 28)
+  expect_that(length(names(data)), not(equals(0)))
 })
 
 test_that("getting account data", {
   data = getData(user="egonw", paging=c(1,10))
   expect_that(length(names(data)), not(equals(0)))
+})
+
+test_that("unfolding of the publication date", {
+  data = getData(user="egonw", paging=c(1,10))
+  expect_that(length(na.omit(data["year"])), not(equals(0)))
 })
